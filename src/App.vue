2b14a4c032acd626d3e7a1b95ad484754c2fde0a
IlievaDayana <73341895+IlievaDayana@users.noreply.github.com>
2021-05-30 17:11:57 +0300

    header

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,14 @@
 <template>
-  <HelloWorld />
+  <Header />
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import Header from "./components/Header.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    Header,
   },
 };
 </script>
---
+++ src/components/Header.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="header">
+       <button>Menu</button>
+       <button>Contacts</button>
+       <button>Info</button>
+    </div>
+</template>
