e527f4341cdfd329b79ab3a4ce02c461484a9497
IlievaDayana <73341895+IlievaDayana@users.noreply.github.com>
2021-05-30 18:20:09 +0300

    footer header styles

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,19 @@
 <template>
+<div class="main-container">
   <Header />
+  <Footer />
+</div>
 </template>
 
 <script>
 import Header from "./components/Header.vue";
+import Footer from "./components/Footer.vue";
 
 export default {
   name: "App",
   components: {
     Header,
+    Footer,
   },
 };
 </script>
